0cc3b06342100278259bf853d3377d9cdb1a63e7
AlexeiA <alexei.a3@gmail.com>
2022-07-20 13:10:00 +0300

    Fix #21

---
+++ src/core/Route.ts
@@ -0,0 +1,43 @@
+import Block from "./Block";
+import { render } from "./renderDOM";
+
+export default class Route {
+	constructor(pathname: string, view: new() => Block, props?: {}) {
+		this._pathname = pathname;
+		this._blockClass = view;
+		this._block = null;
+		this._props = props;
+	}
+
+	private _pathname: string;
+	private _blockClass;
+	private _block: Nullable<Block>;
+	private _props: any;
+
+	navigate(pathname: string) {
+		if (this.match(pathname)) {
+			this._pathname = pathname;
+			this.render();
+		}
+	}
+
+	leave() {
+		if (this._block) {
+			this._block.hide();
+		}
+	}
+
+	match(pathname: string) {
+		return pathname === this._pathname;
+	}
+
+	render() {
+		if (!this._block) {
+			this._block = new this._blockClass();
+			render(this._props.rootQuery, this._block);
+			return;
+		}
+
+		this._block.show();
+	}
+}
--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,46 +1,5 @@
-import { render } from "./renderDOM";
 import Block from "./Block";
-
-export class Route {
-	constructor(pathname: string, view: new() => Block, props?: {}) {
-		this._pathname = pathname;
-		this._blockClass = view;
-		this._block = null;
-		this._props = props;
-	}
-
-	private _pathname: string;
-	private _blockClass;
-	private _block: Nullable<Block>;
-	private _props: any;
-
-	navigate(pathname: string) {
-		if (this.match(pathname)) {
-			this._pathname = pathname;
-			this.render();
-		}
-	}
-
-	leave() {
-		if (this._block) {
-			this._block.hide();
-		}
-	}
-
-	match(pathname: string) {
-		return pathname === this._pathname;
-	}
-
-	render() {
-		if (!this._block) {
-			this._block = new this._blockClass();
-			render(this._props.rootQuery, this._block);
-			return;
-		}
-
-		this._block.show();
-	}
-}
+import Route from "./Route";
 
 class Router {
 	constructor(rootQuery: string) {
