14a610a813cf67211e41befc8e066a19397a0732
AlexeiA <alexei.a3@gmail.com>
2022-07-28 10:59:37 +0300

    Add tests for Button

---
+++ src/core/test/Block.spec.ts
@@ -0,0 +1,31 @@
+import { assert } from "chai";
+import Button from "../../components/button";
+import renderBlock from "../renderBlock";
+
+describe('Тест Block', function () {
+	it ('Попробуем создать кнопку', function () {
+		const button = new Button({text: 'Я кнопка', onClick: () => {}});
+		assert.instanceOf(button, Button);
+	})
+
+	it ('Попробуем отрендерить кнопку', function () {
+		const button = new Button({text: 'Я кнопка', onClick: () => {}});
+		assert.instanceOf(button, Button);
+		document.body.innerHTML = '<div id="app"></div>';
+		renderBlock('#app', button);
+		assert.instanceOf(document.querySelector('button'), HTMLButtonElement);
+	})
+
+	it ('Попробуем кликнуть на кнопку', function () {
+		const button = new Button({text: 'Я кнопка', onClick: () => {
+			document.querySelector('button')!.value = 'Я нажатая кнопка';
+		}});
+		assert.instanceOf(button, Button);
+		document.body.innerHTML = '<div id="app"></div>';
+		renderBlock('#app', button);
+		const buttonElement = document.querySelector('button');
+		assert.instanceOf(buttonElement, HTMLButtonElement);
+		buttonElement!.click();
+		assert.equal(buttonElement!.value, 'Я нажатая кнопка');
+	})
+})
